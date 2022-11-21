5c3135e92d053b3371be2f5a895b2d4bf7537ba9
fouad150 <112832062+fouad150@users.noreply.github.com>
2022-11-21 14:51:09 +0100

    Update scripts.php

--- scripts.php
+++ scripts.php
@@ -12,11 +12,11 @@ function save()
     $category_id = $_POST['category_id'];
     $quantity = $_POST['quantity'];
     $price = $_POST['price'];
-    global $image_destination;
-    saveImage();
+    // global $image_destination;
+    // saveImage();
     global $con;
-    $sql = "INSERT INTO `instruments`(`name`, `category_id`, `quantity`, `price`,`image`) VALUES 
-    ('$name','$category_id','$quantity','$price','$image_destination')";
+    $sql = "INSERT INTO `instruments`(`name`, `category_id`, `quantity`, `price`) VALUES 
+    ('$name','$category_id','$quantity','$price')";
     mysqli_query($con, $sql);
     $_SESSION['successful-adding'] = "The instrument has been added successfully!   ";
     header('location: index.php');
@@ -29,7 +29,7 @@ function getInstrument()
     $result = mysqli_query($con, $sql);
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<tr href='#modal-instrument' data-bs-toggle='modal' id='" . $row['id'] . "'  onclick='showModal(this)'>
-                <th scope='row'><img src=" . $row['image'] . " width='150px'></img></th>
+                <th scope='row'>#</th>
                 <td>" . $row['name'] . "</td>
                 <td id='" . $row['category_id'] . "'>" . $row['ctg'] . "</td>
                 <td>" . $row['quantity'] . "</td>
@@ -45,12 +45,10 @@ function update()
     $category_id = $_POST['category_id'];
     $quantity = $_POST['quantity'];
     $price = $_POST['price'];
-    global $image_destination;
-    // var_dump($image_destination);
-    saveImage();
-    // var_dump($image_destination);
+    // global $image_destination;
+    // saveImage();
     global $con;
-    $sql = "UPDATE `instruments` SET `name`='$name',`category_id`='$category_id',`quantity`='$quantity',`price`='$price',`image`='$image_destination'
+    $sql = "UPDATE `instruments` SET `name`='$name',`category_id`='$category_id',`quantity`='$quantity',`price`='$price' 
     WHERE `id`='$instrument_id'";
     mysqli_query($con, $sql);
     header('location: index.php');
@@ -80,28 +78,29 @@ function countInstruments()
     echo $instrument_number;
 }
 
-function saveImage()
-{
-    $image_name = $_FILES['image']['name'];
-    if (empty($image_name)) {
-        exit();
-    }
-    $image_tmp_name = $_FILES['image']['tmp_name'];
-    $image_error = $_FILES['image']['error'];
+// function saveImage()
+// {
+//     $image_name = $_FILES['image']['name'];
+//     if (empty($image_name)) {
+//         exit();
+//     }
+//     $image_tmp_name = $_FILES['image']['tmp_name'];
+//     $image_error = $_FILES['image']['error'];
 
-    $exploded_image = explode('.', $image_name);
-    $image_extension = strtolower(end($exploded_image));
-    $available_extensions = array("jpg", "jpeg", "png");
-    global $image_destination;
-    if (in_array($image_extension, $available_extensions)) {
-        if ($image_error == 0) {
-            $new_unique_name = uniqid('', true) . "." . $image_extension;
-            $image_destination = 'Instruments-images/' . $new_unique_name;
-            move_uploaded_file($image_tmp_name, $image_destination);
-        } else {
-            $_SESSION['err-uplaoding_img'] = "TRY AGAIN !";
-        }
-    } else {
-        $_SESSION['err-uplaoding_img'] = "can not upload image of extention .$image_extension ! (only jpg , jpeg , png allowed )";
-    }
-}
+//     $exploded_image = explode('.', $image_name);
+//     $image_extension = strtolower(end($exploded_image));
+//     $available_extensions = array("jpg", "jpeg", "png");
+//     global $image_destination;
+//     if (in_array($image_extension, $available_extensions)) {
+//         if ($image_error == 0) {
+//             $new_unique_name = uniqid('', true) . "." . $image_extension;
+//             $image_destination = 'Instruments-images/' . $new_unique_name;
+//             move_uploaded_file($image_tmp_name, $image_destination);
+//         } else {
+//             $_SESSION['err-uplaoding_img'] = "TRY AGAIN !";
+//         }
+//     } else {
+//         $_SESSION['err-uplaoding_img'] = "can not upload image of extention .$image_extension ! (only jpg , jpeg , png allowed )";
+//     }
+// }
+
